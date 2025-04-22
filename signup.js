05f9b375606d182ef3138708cdda3822dadd193d
Viktoriaria3 <137815390+Viktoriaria3@users.noreply.github.com>
2025-04-22 14:50:06 +0300

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".header_right--mobile img");
+    const menuList = document.querySelector(".header_right--mobile--list");
+  
+    menuButton.addEventListener("click", function (event) {
+      menuList.style.transform = "translateX(0)";
+      event.stopPropagation();
+    });
+  
+    document.addEventListener("click", function (event) {
+      if (
+        !menuList.contains(event.target) &&
+        !menuButton.contains(event.target)
+      ) {
+        menuList.style.transform = "translateX(100%)";
+      }
+    });
+  });
+
+  const btnSignUp = document.getElementById("button");
+  
+  btnSignUp.addEventListener("click", function (event) {
+    window.location.href = "thanks.html"
+  });
