c2856f25c6bf22ef855207cbb4a5d93e1d47bba6
Mariia Bohachenko <mariiabohachenko@gmail.com>
2025-05-19 23:37:20 +0300

    back-end final modification

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
-import MySQLdb
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
 import mysql.connector
-#import MySQL, MySQLdb
+import MySQLdb, mysql.connector
 
 app = Flask(__name__)
 
-events = [
-    {
-        'todo' : 'Tutorial for Ann',
-        'date' : '2025-05-05',
-    },
-    {
-        'todo' : 'Mathematics exam',
-        'date' : '2025-05-23',
-    }
-]
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1201'
+app.config['MYSQL_DB'] = 'cybersisters'
 
-# MySQL config
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="1201",
-    database="cybersisters"
-)
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route ('/calendar')
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        deadline = request.form['deadline']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO event (title, content, deadline) VALUES (%s, %s, %s)", (title, content, deadline))
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+
+@app.route('/calendar')
 def calendar():
-    return render_template('calendar.html', events = events)
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM events ORDER BY ID")
+    event = cur.fetchall()
+    return render_template('calendar.html', event=event)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
